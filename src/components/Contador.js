ccb06116f5192541ce01152c2fdf7e21afc63557
Marina <marinavila.mv@gmail.com>
2023-01-09 12:57:52 +0100

    contador-clicks

---
+++ src/components/Contador.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style-sheets/Contador.css";
+
+function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
+}
+
+export default Contador;
